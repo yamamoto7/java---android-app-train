62304e1e53877aa52eab04a2ad0d3e582fe7203e
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-08-30 05:28:36 +0900

    Edit text

--- AppTrain2/app/src/main/java/com/example/k7287/apptrain2/MainActivity.java
+++ AppTrain2/app/src/main/java/com/example/k7287/apptrain2/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +19,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) { // import android.view.View;が必要
                 // import android.widget.Toast;が必要
                 Toast.makeText(MainActivity.this, "ボタンが押されました。", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        ImageButton button2 = (ImageButton) findViewById(R.id.imageButton);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "画像ボタンが押されました。", Toast.LENGTH_SHORT)
                         .show();
             }
         });
