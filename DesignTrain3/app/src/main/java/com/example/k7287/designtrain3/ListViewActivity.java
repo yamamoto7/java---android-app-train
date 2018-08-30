4eb399b9cac8ea3d204ed3e54ff3c91f9f8bda25
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-08-30 17:12:59 +0900

    DesignTrain3 initial commit

---
+++ DesignTrain3/app/src/main/java/com/example/k7287/designtrain3/ListViewActivity.java
@@ -0,0 +1,11 @@
+package com.example.k7287.designtrain3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ListViewActivity extends AppCompatActivity {
+    protected void onCreated(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.list_view);
+    }
+}
