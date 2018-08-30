9dbd2178c822a19310e66710d26f288bd8d36dca
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-08-30 15:46:28 +0900

    add pager (another activity)

--- AppTrain2/app/src/main/java/com/example/k7287/apptrain2/MainActivity.java
+++ AppTrain2/app/src/main/java/com/example/k7287/apptrain2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.k7287.apptrain2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -43,6 +44,16 @@ public class MainActivity extends AppCompatActivity {
                 setScreenSub();
             }
         });
+
+        Button pagerButton2 = (Button)findViewById(R.id.pagerButton2);
+        pagerButton2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
     private void setScreenSub(){
---
+++ AppTrain2/app/src/main/java/com/example/k7287/apptrain2/SubActivity.java
@@ -0,0 +1,22 @@
+package com.example.k7287.apptrain2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class SubActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+//
+//        Button returnButton = (Button) findViewById(R.id.returnButton);
+//        returnButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+//            }
+//        });
+    }
+}
